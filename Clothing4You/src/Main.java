ccf8ccb6dbca50b25f10d338642e0b30c102f32b
Neha <nvj98@my.yorku.ca>
2023-02-07 18:11:29 -0500

    MySql Database connection

--- Clothing4You/src/Main.java
+++ Clothing4You/src/Main.java
@@ -1,3 +1,15 @@
-public class Main {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+
+public class Main {
+    public static void main(String[] args){
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql", "root", "EECS3311");
+            System.out.println(con + "\n" + "Connection successful!");
+        }
+        catch(Exception e){
+            System.out.println("Connection failed!");
+        }
+    }
 }
