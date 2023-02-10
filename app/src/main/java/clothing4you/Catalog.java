6d7eb940976264d213a9729f4ce45eb772a90f1a
gabikir99 <99775013+gabikir99@users.noreply.github.com>
2023-02-10 11:25:09 -0500

    Update Catalog.java

--- app/src/main/java/clothing4you/Catalog.java
+++ app/src/main/java/clothing4you/Catalog.java
@@ -27,6 +27,8 @@ public class Catalog extends JDialog {
         setModal(true);
         setLocationRelativeTo(parent);
 
+        cart = new Cart();
+
         items = new ArrayList<>();
         Image tShirt = new ImageIcon("Tshirt.png").getImage();
         Image hoodie = new ImageIcon("Hoodie.png").getImage();
@@ -34,12 +36,12 @@ public class Catalog extends JDialog {
         Image shorts = new ImageIcon("Shorts.png").getImage();
         Image beanie = new ImageIcon("Beanie.png").getImage();
         Image hat = new ImageIcon("Hat.png").getImage();
-        items.add(new Item("T-shirt", "Tops", "M",10.0, tShirt));
-        items.add(new Item("Hoodie", "Tops", "M",20.0, hoodie));
-        items.add(new Item("Jeans", "Bottoms", "M", 30.0, jeans));
-        items.add(new Item("Shorts", "Bottoms", "M",10.0, shorts));
-        items.add(new Item("Beanie", "Hats", "M",10.0, beanie));
-        items.add(new Item("Hat", "Hats", "M",10.0, hat));
+        items.add(new Item("T-shirt", "Tops", "M", 1, 20.00, tShirt));
+        items.add(new Item("Hoodie", "Tops", "M",1,25.00, hoodie));
+        items.add(new Item("Jeans", "Bottoms", "M",1, 20.00, jeans));
+        items.add(new Item("Shorts", "Bottoms", "M",1,15.00, shorts));
+        items.add(new Item("Beanie", "Hats", "M",1,7.50, beanie));
+        items.add(new Item("Hat", "Hats", "M",1,7.50, hat));
 
 
 
@@ -48,7 +50,6 @@ public class Catalog extends JDialog {
         model.addColumn("Size");
         model.addColumn("Price");
         model.addColumn("Image");
-        model.addColumn("");
         table = new JTable(model);
         JScrollPane scrollPane = new JScrollPane(table);
         catalogPanel.add(scrollPane, BorderLayout.CENTER);
@@ -88,28 +89,32 @@ public class Catalog extends JDialog {
         checkout.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                dispose();
+                OrderSummary mySummary = new OrderSummary(null, cart.getItems());
             }
         });
         button.add(checkout);
         catalogPanel.add(button, BorderLayout.SOUTH);
 
-        table.getColumnModel().getColumn(4).setCellRenderer(new DefaultTableCellRenderer() {
+        JPanel button1 = new JPanel();
+        JButton addToCart = new JButton("Add To Cart");
+        addToCart.addActionListener(new ActionListener() {
             @Override
-            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-                JButton addToCart = new JButton("Add to Cart");
-                addToCart.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        int row = table.getSelectedRow();
-                        Item item = items.get(row);
-                        cart.addItem(item);
-                    }
-
-                });
-                return addToCart;
+            public void actionPerformed(ActionEvent e) {
+                int row = table.getSelectedRow();
+                if (row != -1) {
+                    Item item = items.get(row);
+                    cart.addItem(item);
+                    JOptionPane.showMessageDialog(catalogPanel, item.getName() + " added to cart.");
+                } else {
+                    JOptionPane.showMessageDialog(catalogPanel, "Please select an item that is available.");
+                }
             }
         });
+        button1.add(addToCart);
+        catalogPanel.add(button1, BorderLayout.EAST);
+
+
         setVisible(true);
         updateTable();
 
@@ -120,7 +125,7 @@ public class Catalog extends JDialog {
         String selectedCategory = (String) cmCategory.getSelectedItem();
         for (Item item : items) {
             if (selectedCategory.equals("All") || selectedCategory.equals(item.getCategory())) {
-                model.addRow(new Object[] { item.getName(), item.getSize(), item.getPrice(), item.getImage() });
+                model.addRow(new Object[] { item.getName(), item.getSize(), "$" + String.format("%.2f",item.getPrice()), item.getImage() });
             }
         }
     }
