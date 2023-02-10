23b00ed801b7e7c2c631856892ac9f71aee0bc14
gabikir99 <99775013+gabikir99@users.noreply.github.com>
2023-02-10 11:20:44 -0500

    Create OrderSummary.java

---
+++ app/src/main/java/clothing4you/OrderSummary.java
@@ -0,0 +1,69 @@
+package clothing4you;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class OrderSummary extends JDialog{
+    private JPanel orderSummaryPanel;
+    private ArrayList<Item> items;
+    private JTable table;
+    private DefaultTableModel model;
+    private Cart cart;
+
+    public OrderSummary(JFrame parent, ArrayList<Item> items){
+        super(parent);
+        setTitle("Order Summary");
+        orderSummaryPanel = new JPanel(new BorderLayout());
+        setContentPane(orderSummaryPanel);
+        setMinimumSize(new Dimension(600, 600));
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setModal(true);
+        setLocationRelativeTo(parent);
+
+        this.items = items;
+        cart = new Cart();
+
+        String[] column = {"Name", "Price"};
+        model = new DefaultTableModel(column, 0);
+
+        table = new JTable(model);
+        int totalQuantity = 0;
+        for (Item item: items){
+            cart.addItem(item);
+            model.addRow(new Object[]{item.getName(), "$" + String.format("%.2f",item.getPrice())});
+        }
+        model.addRow(new Object[]{"Total Price:", "$" + String.format("%.2f",cart.getTotal())});
+
+        orderSummaryPanel.add(new JScrollPane(table), BorderLayout.CENTER);
+
+        JPanel button = new JPanel();
+        JButton back = new JButton("Back");
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                Catalog myCatalog = new Catalog(null);
+            }
+        });
+        button.add(back);
+
+        JButton checkout = new JButton("CheckOut");
+        checkout.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                CheckOut myCheckOut = new CheckOut(null);
+            }
+        });
+        button.add(checkout);
+        orderSummaryPanel.add(button, BorderLayout.SOUTH);
+
+
+
+        setVisible(true);
+    }
+}
