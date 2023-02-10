e5fb2fc079a54f5e1baa9d075223ba19ddb370de
andrijaradiv <50723531+andrijaradiv@users.noreply.github.com>
2023-02-10 18:02:12 -0500

    Added exists method

--- app/src/main/java/clothing4you/JDBC.java
+++ app/src/main/java/clothing4you/JDBC.java
@@ -1,56 +1,101 @@
+package clothing4you;
+
 import java.sql.*;
+import java.util.ArrayList;
 
 public class JDBC {
-    static String url = "jdbc:mysql://sql9.freesqldatabase.com:3306/sql9596544";
-    static String username = "sql9596544";
-    static String password = "KLHarcxrhe";
-    public static ResultSet query(String query, String table) {
+    private static String url = "jdbc:sqlite:/sqlite/clothing4you";
+    public static String createUserTable = "CREATE TABLE users (" +
+            "ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "first_name TEXT," +
+            "last_name TEXT," +
+            "email TEXT," +
+            "username TEXT," +
+            "password TEXT" +
+            ");";
 
-        try {
-
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            Connection connection = DriverManager.getConnection(url, username, password);
-
-            Statement statement = connection.createStatement();
-            if(query.toLowerCase().contains("insert")) {
-                int insert = statement.executeUpdate(query + table);
-            }
-            ResultSet resultSet = statement.executeQuery(query + table); //"select * from users"
-
-//            while(resultSet.next()) {
-//                System.out.println(resultSet.getString(1));
-//            }
-
-            return resultSet;
-        } catch(Exception e) {
-            System.out.println(e);
-            return null;
-        }
-
+    public JDBC() throws SQLException, ClassNotFoundException {
     }
 
-    public static ResultSet queryTable(String query, String table) {
+    private static Connection establishConnection() throws SQLException, ClassNotFoundException {
+        // Establishes connection with sqlite database
+
+        Connection conn = null;
+
         try {
-            ResultSet resultSet = query(query, table);
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:clothing4you.db");
 
-            return resultSet;
-
-        } catch(Exception e) {
-            return null;
+        } catch ( Exception e ) {
         }
-
-    }
-    public static ResultSet getTable(String table) {
-        try {
-            ResultSet resultSet = query("select * from ", table);
-
-            return resultSet;
-
-        } catch(Exception e) {
-            return null;
-        }
-
+        return conn;
     }
 
+    public static void createTable(String table, String createTable) throws SQLException, ClassNotFoundException {
+        // Creates table in sqlite database with table name input and sql create table statement
+
+        Connection conn = establishConnection();
+        Statement stmt = conn.createStatement();
+
+        stmt.executeUpdate("drop table if exists " + table +";");
+        stmt.executeUpdate(createTable);
+        stmt.close();
+        System.out.println("Table created successfully");
+    }
+
+   public static void insertUser(String first_name, String last_name, String email,String username, String password) throws SQLException, ClassNotFoundException {
+        // Inserts user into user table to allow for data query
+
+        Connection conn = establishConnection();
+        conn.setAutoCommit(false);
+        Statement stmt = conn.createStatement();
+        String sqlStatement = "INSERT INTO users (first_name,last_name,email,username,password) " +
+               "VALUES ('" + first_name + "','" + last_name + "','" + email + "','" +username +"','"+ password + "');";
+        stmt.executeUpdate(sqlStatement);
+
+        conn.commit();
+        conn.close();
+   }
+
+    public static ArrayList query(String table, String column_name) throws SQLException, ClassNotFoundException {
+        // Queries given table and returns array with data in column_name
+        ArrayList result = new ArrayList<>();
+
+        Connection conn = establishConnection();
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * from " + table);
+
+        while(resultSet.next()) {
+            result.add(resultSet.getString(column_name));
+            //System.out.println(resultSet.getString("email"));
+        }
+
+        return result;
+    }
+
+    public static boolean exists(String data, String table, String column) throws SQLException, ClassNotFoundException {
+        // Checks if data in column in table exists, returns true if it exists and false if it does not
+
+        boolean exists = false;
+        String sqlSearch = "SELECT " + column + " " +
+                "FROM " + table + " " +
+                "WHERE EXISTS " +
+                "(SELECT " + column + " FROM " + table + " WHERE " + column + " = '" + data + "');";
+
+        Connection conn = establishConnection();
+        Statement stmt = conn.createStatement();
+        ResultSet res = stmt.executeQuery(sqlSearch);
+        exists = res.next();
+
+        return exists;
+    }
+    // For reference
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//        createTable("users", createUserTable);
+//        insertUser("andrija", "rad", "a@gmail.com","andrija121", "1234");
+//        insertUser("willy", "lego", "willlego@gmail.com","willo1053", "1234");
+//        ArrayList a = query("users", "username");
+//        System.out.println(a);
+//        System.out.println(exists("wow14266", "users", "username"));
+    }
 }
